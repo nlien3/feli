48c02af62dc56f6fee5d9fec9db59174580b0c41
arnonahmias <2222270@ucc.edu.ar>
2024-07-15 15:34:20 -0300

    borro imagen

--- backend/clients/db_clients.go
+++ backend/clients/db_clients.go
@@ -18,7 +18,7 @@ var DB *gorm.DB
 
 func InitDB() {
 	log.Println("Initializing database...")
-	dsn := "root:@tcp(127.0.0.1:3306)/proyecto4?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:admin@tcp(127.0.0.1:3306)/proyecto?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -54,25 +54,27 @@ func SeedDB() {
 	DB.FirstOrCreate(&admin, dao.Usuario{NombreUsuario: "admin"})
 	DB.FirstOrCreate(&user, dao.Usuario{NombreUsuario: "user"})
 
-	// Crear un nuevo usuario sin hashear la contraseña
-	plainPassword := "queso"
-	quesoUser := dao.Usuario{NombreUsuario: "queso", Contrasena: plainPassword, Tipo: "normal"}
-
-	DB.FirstOrCreate(&quesoUser, dao.Usuario{NombreUsuario: "queso"})
-
 	log.Println("Database seeded successfully")
-/*
+
 	cursos := []dao.Course{
 		{Nombre: "Ingles B2", Categoria: "Idiomas", Dificultad: "Medio", Precio: 45, Descripcion: "Curso de ingles avanzado para aprobar tus examenes internacionales", ImageURL: "https://diarium.usal.es/ireneigls/files/2018/09/b2-de-ingles.jpg"},
 		{Nombre: "Hacking Etico", Categoria: "Programación", Dificultad: "Dificil", Precio: 60, Descripcion: "Curso de como ser hacker sin ser hacker", ImageURL: "https://www.pmg-ssi.com/wp-content/uploads/2023/08/he.jpg"},
+		{Nombre: "Rainbow six", Categoria: "Videojuegos", Dificultad: "Muy Dificil", Precio: 100, Descripcion: "Curso de como aprender a jugar al mejor juego de Ubisoft", ImageURL: "https://media.tycsports.com/files/2021/08/13/319515/rainbow-six-siege-gratis_1440x810_wmk.jpg"},
+		{Nombre: "Padel principiante", Categoria: "Deportes", Dificultad: "Facil", Precio: 20, Descripcion: "Curso de como aprender padel en 4 meses", ImageURL: "https://upload.wikimedia.org/wikipedia/commons/6/6c/Agustin-tapia.jpg"},
+		{Nombre: "Simulador iracing", Categoria: "Videojuegos", Dificultad: "Muy difil", Precio: 30, Descripcion: "Curso para parecerse a Max Verstappen", ImageURL: "https://i.blogs.es/4b09e7/captura-de-pantalla-2022-05-25-a-las-13.48.10/1366_2000.jpeg"},
+		{Nombre: "Fotografia", Categoria: "Estilo de vida", Dificultad: "Medio", Precio: 62, Descripcion: "Curso de como hacer fotografias para diarios y revistas", ImageURL: "https://pqs.pe/wp-content/uploads/2015/10/pqs-fotografia.jpg"},
+		{Nombre: "Cocteleria", Categoria: "Estilo de vida", Dificultad: "Facil", Precio: 18, Descripcion: "Curso pricipiante de como hacer los coctele mas faciles de argentina", ImageURL: "https://media.revistagq.com/photos/5df397b1b1280d00088d986e/16:9/w_2560%2Cc_limit/iStock-680866296.jpg"},
+		{Nombre: "Cocina", Categoria: "Estilo de vida", Dificultad: "Medio", Precio: 30, Descripcion: "Curso de prepareacion para MASTERCHEF", ImageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqbKsTAP_QuCXJ3kfxrwVSvSy2VgW-QJY1kA&s"},
+		{Nombre: "Ceramica", Categoria: "Manualidades", Dificultad: "dificil", Precio: 23, Descripcion: "Curso provincial de ceramica familiar", ImageURL: "https://www.conasi.eu/blog/wp-content/uploads/2020/09/ceramica-y-porcelana-para-cocinar-1111-1.jpg"},
+		{Nombre: "FIFA", Categoria: "Videojuegos", Dificultad: "Facil", Precio: 10, Descripcion: "Curso de como jugar al FIFA de manera profesional", ImageURL: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/SWFBFT2L5JDJTD4KKOYIU6FDZY.jpg"},
 	}
+
 	for _, curso := range cursos {
 		DB.Create(&curso)
 	}
 	log.Println("Database seeded successfully")
-	*/
-}
 
+}
 
 func CreateUser(nombreUsuario, contrasena, tipo string) error {
 	user := dao.Usuario{
--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,7 @@ go 1.22.3
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/mattn/go-sqlite3 v1.14.22
 	gorm.io/driver/mysql v1.5.6
@@ -16,7 +17,6 @@ require (
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
-	github.com/gin-contrib/cors v1.7.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -27,6 +27,7 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
